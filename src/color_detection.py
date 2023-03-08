f0d4d4a0ae674c98ebb1a5478270c755c138b8ee
bridgetg19 <60526814+bridgetg19@users.noreply.github.com>
2023-03-07 18:32:39 -0600

    prints flattened view of cube after it scans

--- src/color_detection.py
+++ src/color_detection.py
@@ -1,8 +1,8 @@
 import cv2
-import qrcode
+# import qrcode
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import matplotlib.pyplot as plt
 from PIL import Image
 from pyzbar.pyzbar import decode
 
@@ -122,11 +122,6 @@ def start_video():
                 
                 face_count += 1 #increment
                 
-                # cv2.imwrite("img/cam_ss.png", frame)
-                # print(get_rubiks_face(decodedObjects))
-
-                # break
-
 
             for d in decodedObjects:
                 s = d.data.decode()
@@ -162,9 +157,57 @@ def start_video():
 
 
 
+#takes an image, decodes it, and returns it as a string like 'BBBBBBBBB'
+def decode_images():
+    f1 = get_rubiks_face(detect_multiple_qr(cv2.imread('img/face_1.png')))
+    f2 = get_rubiks_face(detect_multiple_qr(cv2.imread('img/face_2.png')))
+    f3 = get_rubiks_face(detect_multiple_qr(cv2.imread('img/face_3.png')))
+    f4 = get_rubiks_face(detect_multiple_qr(cv2.imread('img/face_4.png')))
+    f5 = get_rubiks_face(detect_multiple_qr(cv2.imread('img/face_5.png')))
+    f6 = get_rubiks_face(detect_multiple_qr(cv2.imread('img/face_6.png')))
+
+    return f1, f2, f3, f4, f5, f6
+
+
+
+#prints out flattened view of the cube, just to verify quickly
+def chunk_face(f):
+    return [f[x:x+3] for x in range(0, len(f), 3)]
+
+def net_cube(f1, f2, f3, f4, f5, f6):
+    top = chunk_face(f1)
+    print(" "*3, top[0])
+    print(" "*3, top[1])
+    print(" "*3, top[2])
+
+    left = chunk_face(f5)
+    front = chunk_face(f3)
+    right = chunk_face(f2)
+    back = chunk_face(f6)
+    print(left[0], front[0], right[0], back[0])
+    print(left[1], front[1], right[1], back[1])
+    print(left[2], front[2], right[2], back[2])
+
+    down = chunk_face(f4)
+    print(" "*3, down[0])
+    print(" "*3, down[1])
+    print(" "*3, down[2])
+
+
 
 def main():
+    #start video to get all 6 sides
     start_video()
+
+    #decode all 6 images and get string representation of decoded data
+    f1, f2, f3, f4, f5, f6 = decode_images()
+
+    #print out what it read and verify
+    net_cube(f1, f2, f3, f4, f5, f6)
+
+    #send to kociemba algorithm
+    #functionality in python notebook, add once everything else works
+
 
 if __name__ == "__main__":
     main()
