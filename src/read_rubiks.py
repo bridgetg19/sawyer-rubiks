0a5168ce335795e2fef149d5e2813e62d6b43dcb
bridgetg19 <60526814+bridgetg19@users.noreply.github.com>
2023-04-14 11:55:51 -0500

    changed template and coordinates

--- src/read_rubiks.py
+++ src/read_rubiks.py
@@ -7,11 +7,11 @@ from color_detect import *
 from kociemba_stuff import *
 
 # --- global variables -------------------------------------------------------
-x1, x2 = 325, 955 #starting & ending x coordinate of grid template
-y1, y2 = 45, 675 #starting & ending y coordinate of grid template
-cell_size = 210 #size of cell from grid template
-cx = 640 #center x coordinate of frame
-cy = 360 #center y coordinate of frame
+x1, x2 = 425, 900 #starting & ending x coordinate of grid template
+y1, y2 = 150, 600 #starting & ending y coordinate of grid template
+cell_size = 158 #size of cell from grid template
+cx = 662 #center x coordinate of frame
+cy = 382 #center y coordinate of frame
 
 
 # gets the center pixel coordinate of each cell in the 3x3 grid templae
@@ -34,33 +34,55 @@ def get_cell_centers():
 
 # draws the 3x3 grid template on frame in video
 def draw_template(frame):
-    color=(0,0,255)
+    color=(255,0,0)
     thickness = 5
-
-    cv2.line(frame, (x1,y1), (x2,y1), color, thickness) #top
-    cv2.line(frame, (x1,y1), (x1,y2), color, thickness) #left
-    cv2.line(frame, (x1,y2), (x2,y2), color, thickness) #bottom
-    cv2.line(frame, (x2,y1), (x2,y2), color, thickness) #right
-
-    cv2.line(frame, (x1, y1+cell_size), (x2, y1+cell_size), color, thickness) #horizontal 1
-    cv2.line(frame, (x1, y1+(cell_size*2)), (x2, y1+(cell_size*2)), color, thickness) #horizontal 2
-    cv2.line(frame, (x1+cell_size, y1), (x1+cell_size, y2), color, thickness) #vertical 1
-    cv2.line(frame, (x1+(cell_size*2), y1), (x1+(cell_size*2), y2), color, thickness) #vertical 2
-
-    height, width, _ = frame.shape
-    cx = width // 2
-    cy = height // 2
+    color_blue = (0,255,0)
 
     # cv2.circle(image, center_coordinates, radius, color, thickness)
+    #top row
+    cv2.circle(frame, (cx - cell_size - 30, cy - cell_size - 30), 10, color_blue, thickness)
+    cv2.circle(frame, (cx, cy - cell_size - 30), 10, color_blue, thickness)
+    cv2.circle(frame, (cx + cell_size + 30, cy - cell_size - 30), 10, color_blue, thickness)
+
+    #middle row
+    cv2.circle(frame, (cx - cell_size - 30, cy), 10, color_blue, thickness)
     cv2.circle(frame, (cx, cy), 10, color, thickness)
-    cv2.circle(frame, (cx + cell_size, cy), 10, color, thickness)
-    cv2.circle(frame, (cx - cell_size, cy), 10, color, thickness)
-    cv2.circle(frame, (cx, cy + cell_size), 10, color, thickness)
-    cv2.circle(frame, (cx + cell_size, cy + cell_size), 10, color, thickness)
-    cv2.circle(frame, (cx - cell_size, cy + cell_size), 10, color, thickness)
-    cv2.circle(frame, (cx, cy - cell_size), 10, color, thickness)
-    cv2.circle(frame, (cx + cell_size, cy - cell_size), 10, color, thickness)
-    cv2.circle(frame, (cx - cell_size, cy - cell_size), 10, color, thickness)
+    cv2.circle(frame, (cx + cell_size + 30, cy), 10, color_blue, thickness)
+
+    #bottom row
+    cv2.circle(frame, (cx - cell_size - 30, cy + cell_size + 30), 10, color_blue, thickness)
+    cv2.circle(frame, (cx, cy + cell_size + 30), 10, color_blue, thickness)
+    cv2.circle(frame, (cx + cell_size + 30, cy + cell_size + 30), 10, color_blue, thickness)
+
+
+#prints out flattened out version of cube
+def chunk_face(f):
+    return [f[x:x+3] for x in range(0, len(f), 3)]
+
+def net_cube(f1, f2, f3, f4, f5, f6):
+    top = chunk_face(f1)
+    print(" "*7, "- "*5)
+    print(" "*7, "|", top[0][0], top[0][1], top[0][2], "|")
+    print(" "*7, "|", top[1][0], top[1][1], top[1][2], "|")
+    print(" "*7, "|", top[2][0], top[2][1], top[2][2], "|")
+    print(" "*7, "- "*5)
+
+    left = chunk_face(f5)
+    front = chunk_face(f3)
+    right = chunk_face(f2)
+    back = chunk_face(f6)
+    print("- "*17)
+    print("|", left[0][0], left[0][1], left[0][2], "|", front[0][0], front[0][1], front[0][2], "|", right[0][0], right[0][1], right[0][2], "|", back[0][0], back[0][1], back[0][2], "|")
+    print("|", left[1][0], left[1][1], left[1][2], "|", front[1][0], front[1][1], front[1][2], "|", right[1][0], right[1][1], right[1][2], "|", back[1][0], back[1][1], back[1][2], "|")
+    print("|",left[2][0], left[2][1], left[2][2], "|", front[2][0], front[2][1], front[2][2], "|", right[2][0], right[2][1], right[2][2], "|", back[2][0], back[2][1], back[2][2], "|")
+    print("- "*17)
+
+    down = chunk_face(f4)
+    print(" "*7, "- "*5)
+    print(" "*7, "|", down[0][0], down[0][1], down[0][2], "|")
+    print(" "*7, "|", down[1][0], down[1][1], down[1][2], "|")
+    print(" "*7, "|", down[2][0], down[2][1], down[2][2], "|")
+    print(" "*7, "- "*5)
 
 
 
@@ -120,6 +142,9 @@ def main():
 
     #replace center tile of face 1 with "W" bc of logo
     f1[4] = 'W'
+
+    #print out cube state
+    net_cube(f1, f2, f3, f4, f5, f6)
 
     #concatenate all the faces together
     rubiks_cube = list_to_string(f1) + list_to_string(f2) + list_to_string(f3) + list_to_string(f4) + list_to_string(f5) + list_to_string(f6)
